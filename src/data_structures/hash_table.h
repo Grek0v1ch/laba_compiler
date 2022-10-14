6d896b234218d36315cb128f79463e9a76277976
Ivan Voronov <ivvoronov2308@gmail.com>
2022-10-14 22:18:16 +0300

    Написан класс хеш-таблицы. Реализованы методы добавления элемента и печать таблицы в консоль

---
+++ src/data_structures/hash_table.h
@@ -0,0 +1,68 @@
+#ifndef HASH_TABLE_H
+#define HASH_TABLE_H
+
+#include <string>
+#include <vector>
+#include "linked_list.h"
+
+// Хеш-функция для работы со строками.
+int hash_function_horner(const std::string& s, const int table_size, const int key) {
+    int hash_result = 0;
+    for (unsigned int i = 0; i < s.length(); i++) {
+        hash_result = (key * hash_result + s[i]) % table_size;
+    }
+    hash_result = (hash_result * 2 + 1) % table_size;
+    return hash_result;
+}
+
+// Структура - обертка над хеш-функцией.
+struct hash_function {
+	static const int _key = 53;
+    int operator()(const std::string& s, const int table_size) const {
+        return hash_function_horner(s, table_size, _key);
+    }
+};
+
+// Класс хеш-таблицы.
+// В качестве второго передаваемого класса следует указатель класс-обертку над хеш-функцией.
+// В классе обертке необходима переалресация оператора ().
+template <class T, class T_hash = hash_function>
+class hash_table {
+private:
+	static const int DEFAULT_SIZE = 7;
+	std::vector<std::shared_ptr<linked_list<T>>> _arr;
+	unsigned int _size;
+public:
+	hash_table() : _arr(std::vector<std::shared_ptr<linked_list<T>>>(DEFAULT_SIZE, 0)), _size(0) {}
+
+	// Метод добавления элемента в хеш-таблицу.
+	void add(const T& value) {
+		T_hash hash = T_hash();
+		// Вычисляем хеш элемента.
+		int idx = hash(value, _arr.size());
+		if (_arr[idx] == 0) {
+			_size++;
+			_arr[idx] = std::make_shared<linked_list<T>>(linked_list<T>(value));
+			return;
+		}
+		if (_arr[idx]->find(value)) {
+			return;
+		}
+		_size++;
+		_arr[idx]->push_front(value);
+	}
+
+	// Метод печати хеш-таблицы в консоль.
+	void print() {
+		for (unsigned int i = 0; i < _arr.size(); i++) {
+			if (_arr[i] == 0) {
+				continue;
+			}
+			std::cout << "key: " << i << "; values: ";
+			_arr[i]->print();
+			std::cout << '\n';
+		}
+	}
+};
+
+#endif // HASH_TABLE_H
