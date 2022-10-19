00dbe212f1e202480df21b955748728ff0387caa
Ivan Voronov <ivvoronov2308@gmail.com>
2022-10-19 13:59:03 +0300

    Написан черновой вариант основной лабы, весь код в файле main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,32 +2,41 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <cctype>
 #include "data_structures/hash_table.h"
 
 enum type_lex {
-	VAR,
+	KEYWORD,
+	ID,
 	NUM,
-	OPERATION,
-	ERROR,
+	SEPARATORS,
+	STRING,
+	ERROR
 };
 
-enum state {
-	st_start,
-	st_end_var,
-	st_end_num,
-	st_3
-};
-
-enum event {
-	event_letter,
-	event_digit,
-	event_error
-};
-
-using table = std::vector<std::vector<state>>;
-
-class Analize {
+class DFSM {
 private:
+	enum state {
+		st_0_start,
+		st_1_end_id,
+		st_2_end_num,
+		st_3,
+		st_4,
+		st_5_end_string
+	};
+
+	enum event {
+		event_letter,
+		event_digit,
+		event_quotes,
+		event_error
+	};
+
+	using table = std::vector<std::vector<state>>;
+
+	#define COUNT_STATE 6
+	#define COUNT_EVENT 3
+
 	table _table;
 
 	state function_step(const state from, const event curr_event) const {
@@ -38,32 +47,47 @@ private:
 	}
 
 	event get_event(const char symbol) const {
-		if ('a' <= symbol && symbol <= 'z') {
+		if (isalpha(symbol)) {
 			return event_letter;
 		}
-		if ('0' <= symbol && symbol <= '9') {
+		if (isdigit(symbol)) {
 			return event_digit;
+		}
+		if (symbol == '"') {
+			return event_quotes;
 		}
 		return event_error;
 	}
 
 public:
-	Analize() : _table(table(4, std::vector<state>(2))) {
-		_table[0][0] = st_end_var;
-		_table[0][1] = st_end_num;
+	DFSM() : _table(table(COUNT_STATE, std::vector<state>(COUNT_EVENT))) {
+		_table[st_0_start][event_letter] = st_1_end_id;
+		_table[st_0_start][event_digit] = st_2_end_num;
+		_table[st_0_start][event_quotes] = st_3;
 
-		_table[1][0] = st_end_var;
-		_table[1][1] = st_end_var;
+		_table[st_1_end_id][event_letter] = st_1_end_id;
+		_table[st_1_end_id][event_digit] = st_4;
+		_table[st_1_end_id][event_quotes] = st_4;
 
-		_table[2][0] = st_3;
-		_table[2][1] = st_end_num;
+		_table[st_2_end_num][event_letter] = st_4;
+		_table[st_2_end_num][event_digit] = st_2_end_num;
+		_table[st_2_end_num][event_quotes] = st_4;
 
-		_table[3][0] = st_3;
-		_table[3][1] = st_3;
+		_table[st_3][event_letter] = st_3;
+		_table[st_3][event_digit] = st_3;
+		_table[st_3][event_quotes] = st_5_end_string;
+
+		_table[st_4][event_letter] = st_4;
+		_table[st_4][event_digit] = st_4;
+		_table[st_4][event_quotes] = st_4;
+
+		_table[st_5_end_string][event_letter] = st_4;
+		_table[st_5_end_string][event_digit] = st_4;
+		_table[st_5_end_string][event_quotes] = st_4;
 	}
 
 	type_lex process(const std::string lex) const {
-		state curr_state = st_start;
+		state curr_state = st_0_start;
 		for (char symbol : lex) {
 			event curr_event = get_event(symbol);
 			if (curr_event == event_error) {
@@ -71,13 +95,16 @@ public:
 			}
 			curr_state = function_step(curr_state, curr_event);
 		}
-		if (curr_state == st_end_var) {
-			return VAR;
+		switch (curr_state) {
+			case st_1_end_id:
+				return ID;
+			case st_2_end_num:
+				return NUM;
+			case st_5_end_string:
+				return STRING;
+			default:
+				return ERROR;
 		}
-		if (curr_state == st_end_num) {
-			return NUM;
-		}
-		return ERROR;
 	}
 };
 
@@ -103,20 +130,26 @@ struct lexeme {
 std::ostream& operator<<(std::ostream& out, const lexeme& lex) {
 	std::string output ;
 	switch (lex._id) {
-		case VAR:
-			output = "VAR";
+		case KEYWORD:
+			output = "Keyword";
+			break;
+		case ID:
+			output = "Id";
 			break;
 		case NUM:
-			output = "NUM";
+			output = "Num";
 			break;
-		case OPERATION:
-			output = "OPERATION";
+		case SEPARATORS:
+			output = "Separators";
+			break;
+		case STRING:
+			output = "SimpleStringExpresion";
 			break;
 		case ERROR:
-			output = "ERROR";
+			output = "Error";
 			break;
 	}
-	out << output << "   " << lex._text;
+	out << output << ' ' << lex._text;
 	return out;
 }
 
@@ -138,20 +171,24 @@ struct hash_function_lex {
 };
 
 hash_table<lexeme, hash_function_lex> lex_analize(std::string str) {
-	std::vector<char> space_symbol(3);
-	space_symbol =  {' ', '\n', '\t'};
-	std::vector<char> operation_symbol(2);
-	operation_symbol = {'+', '-'};
+	std::vector<char> space_symbol = { ' ', '\n', '\t' };
+	std::vector<char> separation_symbol = { '+', '-', '(', ')', '{', '}', ',', ';', '=' };
+	std::vector<std::string> keywords = { "return", "int", "char" };
 	hash_table<lexeme, hash_function_lex> res_table;
-	Analize dka = Analize();
+	DFSM automat = DFSM();
 	std::string temp = "";
 	for (size_t i = 0; i < str.size(); i++) {
-		bool is_space_sym = std::find(space_symbol.begin(),
-									  space_symbol.end(), str[i]) != space_symbol.end();
-		bool is_oper_sym = std::find(operation_symbol.begin(),
-									 operation_symbol.end(), str[i]) != operation_symbol.end();
-		if ((is_space_sym || is_oper_sym) && (temp != "")) {
-			type_lex res = dka.process(temp);
+		bool is_space_sym = std::find(space_symbol.begin(), space_symbol.end(),
+									  str[i]) != space_symbol.end();
+		bool is_separation_symbol = std::find(separation_symbol.begin(), separation_symbol.end(),
+											  str[i]) != separation_symbol.end();
+		if ((is_space_sym || is_separation_symbol) && (temp != "")) {
+			if (std::find(keywords.begin(), keywords.end(), temp) != keywords.end()) {
+				res_table.add(lexeme(KEYWORD, temp));
+				temp = "";
+				continue;
+			}
+			type_lex res = automat.process(temp);
 			if (res == ERROR) {
 				std::cout << "Error in " << temp << '\n';
 				temp = "";
@@ -160,11 +197,11 @@ hash_table<lexeme, hash_function_lex> lex_analize(std::string str) {
 			res_table.add(lexeme(res, temp));
 			temp = "";
 		}
-		if (! is_space_sym && ! is_oper_sym) {
+		if (! is_space_sym && ! is_separation_symbol) {
 			temp += str[i];
 		}
-		if (is_oper_sym) {
-			res_table.add(lexeme(OPERATION, str[i]));
+		if (is_separation_symbol) {
+			res_table.add(lexeme(SEPARATORS, str[i]));
 		}
 	}
 	return res_table;
